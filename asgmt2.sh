d904a0fefa1f5414e61b97c5ca8a2c03acf336cb
7788srinath <srinaths119@gmail.com>
2021-07-02 11:43:26 +0530

    fist updated

--- asgmt2.sh
+++ asgmt2.sh
@@ -5,3 +5,4 @@ read number2
 echo "addition of number1 nad number2 is :" `expr $number1 + $number2`
 echo "subraction of number1 nad number2 is :" `expr $number1 - $number2`
 echo "multiplication of number and and number2 :" `expr $number1 \* $number2`
+hi i am srinatha`
